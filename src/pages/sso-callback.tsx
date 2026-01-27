d9caef1035f410d92142b0b76c8c23650531ac81
Emmett <emmettfunstuff@gmail.com>
2026-01-26 22:41:35 -0600

    Ping site

--- src/pages/sso-callback.tsx
+++ src/pages/sso-callback.tsx
@@ -51,8 +51,8 @@ export default function SSOCallbackPage() {
       </Head>
       <div className="mx-auto flex min-h-[calc(100vh-80px)] max-w-7xl items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
         <AuthenticateWithRedirectCallback
-          signInFallbackRedirectUrl="/"
-          signUpFallbackRedirectUrl="/tutor-onboarding"
+          afterSignInUrl="/"
+          afterSignUpUrl="/tutor-onboarding"
         />
         {/* Loading indicator */}
         <div className="flex flex-col items-center">
