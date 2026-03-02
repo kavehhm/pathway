7812387ddf4d36acd36467f462f5b72ccd8c0ec5
Emmett <emmettfunstuff@gmail.com>
2026-03-01 22:15:28 -0600

    bruh

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,8 +25,9 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       <Head>
         <title>Pathway</title>
         <meta name="description" content="Get the best tutoring help, when you need it, where you need it." />
+        <link rel="icon" type="image/png" href="/ourlogo720.png" />
+        <link rel="shortcut icon" href="/ourlogo720.png" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
