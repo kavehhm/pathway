8084c396266b2cba97e98fc64d9e26981fe01029
Emmett <emmettfunstuff@gmail.com>
2026-03-01 22:28:23 -0600

    mickey mouse

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,9 +25,8 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       <Head>
         <title>Pathway</title>
         <meta name="description" content="Get the best tutoring help, when you need it, where you need it." />
-        <link rel="icon" type="image/png" href="/ourlogoresized.png" />
-        <link rel="shortcut icon" href="/ourlogoresized.png" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/ourlogoresized.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
